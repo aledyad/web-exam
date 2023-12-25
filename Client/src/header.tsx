fbaf105fc9b8d2bb69c5ee0bf762fcb539dee7c7
aledyad <aledyad@yandex.ru>
2023-12-25 17:44:01 +0400

    fit height

--- Client/src/header.tsx
+++ Client/src/header.tsx
@@ -6,7 +6,9 @@ import './header.css';
 export default function Header(){
     return(
         <div className={block("header")()}>
-            <img src="/images/favicon.ico" alt="Best shop ever" />
+            <a href="/">
+                <img src="/images/favicon.ico" alt="Best shop ever" />
+            </a>
             <Link to="basket">Корзина</Link>
         </div>
     )
